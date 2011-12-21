9d3198ac59ede5f5dd90b7fff775b9a6864cadfc
Barry Wark <barrywark@gmail.com>
2011-12-21 16:10:31 -0500

    Added PackageMarker class to allow Matlab to find class loader.

---
+++ src/com/physion/ebuilder/PackageMarker.java
@@ -0,0 +1,7 @@
+package com.physion.ebuilder;
+
+/**
+ * Trivially-constructable class that marks the class loader for com.physion.ebuilder. Need from Matlab, unfortunately.
+ */
+public class PackageMarker {
+}
